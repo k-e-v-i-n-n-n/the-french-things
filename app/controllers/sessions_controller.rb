d144c493840f80d40212e11b9e1d08e3de813a17
Kevin Locke <kevinbreid@yahoo.com>
2023-05-04 11:57:14 -0700

    using scope instead of namespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,31 @@
-module Api
-
-class SessionsController < ApplicationController
 
 
-    def create
-        user = User.find_by(username: params[:username])
-        if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user, include: ["words", "expressions","categories.words", "categories.wordstars"], status: :created
-        else
-            render json: {errors: ["Try again :)"]}, status: :unauthorized
+    class SessionsController < ApplicationController
+
+
+        def create
+            user = User.find_by(username: params[:username])
+            if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, include: ["words", "expressions","categories.words", "categories.wordstars"], status: :created
+            else
+                render json: {errors: ["Try again :)"]}, status: :unauthorized
+            end
         end
+
+        def destroy
+            user = User.find_by(id: session[:user_id])
+            if user
+                session.delete(:user_id)
+                head :no_content
+            else
+                render json: {errors: "Unauthorized"}, status: :unauthorized
+            end
+        end
+
+    
     end
 
-    def destroy
-        user = User.find_by(id: session[:user_id])
-        if user
-            session.delete(:user_id)
-            head :no_content
-        else
-            render json: {errors: "Unauthorized"}, status: :unauthorized
-        end
-    end
-
- 
-end
 
 
 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
 
 
-  namespace :api do
+  scope :api do
 
 
   
-  post "/signup", to: "users#create"
+    post "/signup", to: "users#create"
 
-  post "/login", to: "sessions#create"
- 
-  get "/me", to: "users#show"
- 
-  delete "/logout", to: "sessions#destroy"
+    post "/login", to: "sessions#create"
+  
+    get "/me", to: "users#show"
+  
+    delete "/logout", to: "sessions#destroy"
 
-  resources :wordstars
-  resources :categories
-  resources :expressions
-  resources :words
-  resources :users
+    resources :wordstars
+    resources :categories
+    resources :expressions
+    resources :words
+    resources :users
 
-end
-#   # Routing logic: fallback requests for React Router.
-#   # Leave this here to help deploy your app later!
-get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  end
+  #   # Routing logic: fallback requests for React Router.
+  #   # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-# get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && (req.format.html? || req.content_type.include?('application/x-www-form-urlencoded') || req.content_type.include?('multipart/form-data')) }
+  # get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && (req.format.html? || req.content_type.include?('application/x-www-form-urlencoded') || req.content_type.include?('multipart/form-data')) }
 
 end
