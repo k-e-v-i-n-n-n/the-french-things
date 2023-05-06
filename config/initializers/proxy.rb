6413de11e0809bb94ecb2b452dfabe83ab9e05ba
Kevin Locke <kevinbreid@yahoo.com>
2023-05-05 18:20:57 -0700

    resolving infinite loop

--- config/initializers/proxy.rb
+++ config/initializers/proxy.rb
@@ -1,19 +1,23 @@
 require 'rack/proxy'
 
-Rails.application.config.middleware.insert_before 0, Rack::Proxy do
-  # Forward requests to the Express.js server
-  proxy '/exp', to: 'https://the-french-things.onrender.com:8000'
-end
-
-
-
-# Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Proxy do
-#   proxy_options = {
-#     ssl_verify_none: true,
-#     x_forwarded_headers: false,
-#     suppress_via_header: true
-#   }
-
-#   proxy '/translate', 'https://the-french-things.onrender.com:8000', proxy_options
+# Rails.application.config.middleware.insert_before 0, Rack::Proxy do
+#   # Forward requests to the Express.js server
+#   proxy '/exp', to: 'https://the-french-things.onrender.com:8000'
 # end
+
+
+
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Proxy do
+proxy_options = {
+  ssl: {
+    verify: false
+  },
+  headers: {
+    'X-Forwarded-Proto' => 'https'
+  }
+}
+
+
+  proxy '/translate', 'https://the-french-things.onrender.com:8000', proxy_options
+end
 
