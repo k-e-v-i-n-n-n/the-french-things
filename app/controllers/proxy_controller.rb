b35dfed05b6651c38d70f9dd7e0ef5da3c997361
Kevin Locke <kevinbreid@yahoo.com>
2023-05-08 11:13:21 -0700

    updating request method to get

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,9 +1,8 @@
 class ProxyController < ApplicationController
         
     def forward
-          response = HTTParty.send(request.method.downcase, params[:url], headers: request.headers, body: params[:data].to_json)
-          render json: response.body, status: response.code
-        end
-
+        response = HTTParty.send(request.method.downcase, params[:url], headers: request.headers, body: params.except(:url).to_json, port: 8000)
+        render json: response.body, status: response.code
+      end
       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
   end
 
-  post '/translate', to: 'proxy#forward', defaults: { format: :json }
+  get '/translate', to: 'proxy#forward', defaults: { format: :json }
 
 
   # scope :exp do
