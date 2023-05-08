5641cbcca62ea363594eca191c9267cd889dbd00
Kevin Locke <kevinbreid@yahoo.com>
2023-05-08 11:24:52 -0700

    removing port from proxy body

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,7 +1,7 @@
 class ProxyController < ApplicationController
         
     def forward
-        response = HTTParty.send(request.method.downcase, params[:url], headers: request.headers, body: params.except(:url).to_json, port: 8000)
+        response = HTTParty.send(request.method.downcase, params[:url], headers: request.headers, body: params.except(:url).to_json)
         render json: response.body, status: response.code
       end
       
