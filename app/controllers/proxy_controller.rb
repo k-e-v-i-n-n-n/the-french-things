1606dfc24d588cd08b9c66fd7131245dceb69763
Kevin Locke <kevinbreid@yahoo.com>
2023-05-08 11:15:00 -0700

    cleaning up format

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,6 +3,7 @@ class ProxyController < ApplicationController
     def forward
         response = HTTParty.send(request.method.downcase, params[:url], headers: request.headers, body: params.except(:url).to_json, port: 8000)
         render json: response.body, status: response.code
+        
       end
       
 end
